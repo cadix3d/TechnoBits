31124f67fcde72a0e58a89342ac8b1b529f75719
cadix3d <cadix.3d@gmail.com>
2025-08-27 16:44:32 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// create buttons constants 
+
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+	document.body.classList.toggle("show-mobile-menu");
+});
+
+menuCloseButton.addEventListener("click", () => {
+	menuOpenButton.click();
+});
